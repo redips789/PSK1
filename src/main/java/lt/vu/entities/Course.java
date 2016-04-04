fc0856680026da3ba058eeb551fd9118cc455315
redips789 <povilas.jegelevicius@gmail.com>
2016-04-04 13:45:26 +0300

    bandymas

--- src/main/java/lt/vu/entities/Course.java
+++ src/main/java/lt/vu/entities/Course.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates //
  * and open the template in the editor.
  */
-package lt.vu.entities; //
+package lt.vu.entities; 
 
 import java.io.Serializable;
 import java.util.List;
